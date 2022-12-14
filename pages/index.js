dec75698779a4f1f22dd4d7f40010f8812140a51
Sigurdur Svanbergsson <sigurdur.svanbergsson@nordax.se>
2022-12-14 10:40:08 +0000

    Fix casing on link component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import { Popover, Transition } from "@headlessui/react";
 import styles from "../styles/Home.module.css";
 
