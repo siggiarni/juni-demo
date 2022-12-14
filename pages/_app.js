5d8b2922e7ee41b39c00b8977a24a6999ee2ad06
Sigurdur Svanbergsson <sigurdur.svanbergsson@nordax.se>
2022-12-14 15:07:52 +0000

    Fix inter font

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,11 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 function juni({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <div className={inter.className}>
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
 export default juni;
